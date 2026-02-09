2535413bce19e40eb692eccd7082a36623f65e9c
seema <raiseema760@gmail.com>
2026-02-09 14:15:47 +0000

    Reverse digits of a number

--- math_problems.py
+++ math_problems.py
@@ -1,9 +1,6 @@
 print("----------------Count Digits-------------------------")
 # https://takeuforward.org/data-structure/count-digits-in-a-number
 
-
-
-
 class Solution():
     def count_digits_in_a_number(self, digits):
         counter = 0
@@ -25,3 +22,32 @@ class Solution():
         return counter  
 sol = Solution()
 print(sol.count_digits_in_a_number(12345))
+
+
+
+
+print("----------------Reverse digits of a number-------------------------")
+# https://takeuforward.org/maths/reverse-digits-of-a-number
+class Solution():
+    def reverse_integer(self, n):
+
+        INT_MAX = 2**31 - 1
+
+        sign = -1 if n <0 else 1
+        rev_no = 0
+        n = abs(n)
+        while n>0:
+            last_digit = n % 10
+            n = n // 10 
+
+            # if rev_no * 10 + last_digit > INT_MAX:
+            if rev_no > (INT_MAX - last_digit)//10:
+                return 0
+            rev_no = rev_no * 10 + last_digit
+        
+        return rev_no*sign
+    
+
+sol = Solution()
+print(sol.reverse_integer(-123))
+
